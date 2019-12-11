2b5f57bae6562d740f867d847cbb18658474aeb7
Matt Watson <mattwat@gmail.com>
2019-12-11 18:43:19 -0500

    add permutation-generating function and add tests

--- day07.js
+++ day07.js
@@ -3,9 +3,32 @@ const fs = require('fs')
 const input = fs.readFileSync('day07.txt', 'utf8')
 const run = require('./day05')
 
+const permutations = arr => {
+    if (arr.length === 1) {
+        return arr
+    }
+    const perms = []
+    let index = 0
+    while (index < arr.length) {
+        const reducedArr = arr.slice(0, index).concat(arr.slice(index + 1))
+        const reducedArrPerms = permutations(reducedArr)
+        reducedArrPerms.map(perm => [arr[index]].concat(perm)).forEach(perm => {
+            perms.push(perm)
+        })
+        index += 1
+    }
+    return perms
+}
+
+const arr = [...Array(3).keys()]
+const arrPerms = permutations(arr)
+console.log('- array:', arr)
+console.log(`- permutations ${ arrPerms.length }:`, arrPerms)
 // tests
 
 assert(true === true)
+assert.deepEqual(permutations([0,1,2]).map(p => p.toString()), [ '0,1,2', '0,2,1', '1,0,2', '1,2,0', '2,0,1', '2,1,0' ])
+assert(permutations([0,1,2,3,4]).length === 120)
 
 // solution
 
